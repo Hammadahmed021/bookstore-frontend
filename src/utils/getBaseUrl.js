c9df07ac546f824c3b4d5073df227d5eb2a2c589
Hammadahmed021 <hammadvrcreators@gmail.com>
2024-12-11 16:15:00 +0500

    ad

--- src/utils/getBaseUrl.js
+++ src/utils/getBaseUrl.js
@@ -1,3 +1,14 @@
 export const BASE_URL = () => {
-    return import.meta.env.VITE_BASE_URL
-}
+    // Access the mode and environment variables
+    const mode = import.meta.env.MODE; // Gets the current mode (development or production)
+    const env = import.meta.env; // Accesses Vite's environment variables
+  
+    // Determine the base URL based on the mode
+    const baseURL =
+      mode === "production"
+        ? env.VITE_BASE_URL_PRODUCTION
+        : env.VITE_BASE_URL_LOCAL || "/";
+  
+    return baseURL;
+  };
+  
