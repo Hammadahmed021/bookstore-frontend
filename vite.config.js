6b7851351b45c61d3c78e49bf482854c19f9f297
Hammadahmed021 <hammadvrcreators@gmail.com>
2024-12-11 16:20:38 +0500

    ad

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Export the configuration function
-export default defineConfig(({ command, mode }) => {
-  // Load environment variables based on mode
-  const env = loadEnv(mode, process.cwd());
-
-  // Determine the base URL based on the mode
-  const baseURL = mode === 'production'
-    ? env.VITE_BASE_URL_PRODUCTION
-    : env.VITE_BASE_URL_LOCAL || '/';
-
-  return {
-    plugins: [react()],
-    base: baseURL,
-    build: {
-      minify: true,
-      sourcemap: false,
-      target: 'modules',
-    },
-    // define: {
-    //   // Optionally, define any global constants here
-    //   __APP_ENV__: JSON.stringify(env.APP_ENV),
-    // }
-  };
-});
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
