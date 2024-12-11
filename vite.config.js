57c266fd748d9c163961c7ffa26a3486173d6469
Hammadahmed021 <hammadvrcreators@gmail.com>
2024-12-11 17:14:08 +0500

    ad

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,18 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Define a utility function to get the base URL
-const getBaseURL = (mode) => {
-  const env = loadEnv(mode, process.cwd());
-  return mode === 'production' ? env.VITE_BASE_URL_PRODUCTION : env.VITE_BASE_URL_LOCAL || '/';
-};
-
 // Export the configuration
 export default defineConfig(({ mode }) => {
-  const baseURL = getBaseURL(mode);
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd());
 
   return {
     plugins: [react()],
-    base: baseURL,
-    // build: {
-    //   minify: true,
-    //   sourcemap: false,
-    //   target: 'modules',
-    // },
+    base: '/', // Use '/' as the base for Vercel deployment
+    build: {
+      minify: true,
+      sourcemap: false,
+      target: 'modules',
+    },
   };
 });
